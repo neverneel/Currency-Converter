1c2c20e3e5e0036a822144addec8548d557b5f0d
NEEL <neelkamalpunj@gmail.com>
2026-01-30 21:54:47 +0530

    custom hook created

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    useEffect(() => {
+        const [data, setData] = useState({})
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.jsonn())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log();
+    return data
+}
+
+export default useCurrencyInfo;
